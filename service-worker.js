b058ed25f2dc62329dd0e1474c27a956487b3992
Jānis Bedeicis <144352200+loom-framework@users.noreply.github.com>
2026-09-11 17:22:26 +0300

    Update service worker version to 4

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // -----------------------------------------------------------------------------
 
 // --- Versioning -------------------------------------------------------------
-const SW_VERSION = "2"; 
+const SW_VERSION = "4"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
